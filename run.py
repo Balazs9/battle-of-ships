ed6e42e0c50c607863489ec62711bcc82c8e1847
Balazs9 <sempergerbalazs@gmail.com>
2021-09-11 21:56:06 +0000

    add first functions

--- run.py
+++ run.py
@@ -3,17 +3,26 @@ from pprint import pprint
 
 scores = {"computer": 0, "player": 0}
 
-class Board:
-    """
-    Base of the game
-    """
-    def __init__(self, size, num_ships, name, type):
-        self.size = size
-        self.board = [["." for x in range(size)] for y in range(size)]
-        self.num_ships = num_ships
-        self.name = name
-        self.type = type
-        self.guesses = []
-        self.ships = []
-        
+"""
+Base of the game
+"""
 
+board = []
+for x in range(5): 
+    board.append([0]*5)
+
+def __init__(self, size, num_ships, name, type):
+    self.size = size
+    self.board = [["." for x in range(size)] for y in range(size)]
+    self.num_ships = num_ships
+    self.name = name
+    self.type = type
+    self.guesses = []
+    self.ships = []
+    
+def print_board(self, board):
+    for row in self.board:
+        print("".join(row))
+    
+print("Let's play the battle of ships")
+print_board(board)
