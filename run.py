7cc524197a3bf2bd507ce069cce9268d60e79be4
Balazs9 <sempergerbalazs@gmail.com>
2021-09-15 16:18:10 +0000

    Fix problems and solve error in function

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ computer = {
     "wins": 0,
     "lose": 0
 }
-for x in range(0, 5):
+for x in range(1, 6):
     player_board.append(["."] * 5)
     computer_board.append(["."] * 5)
 
@@ -67,17 +67,17 @@ def guess_player_number(player_name):
     """
     Player guess the numbers
     """
-    for game in range(6):
+    for game in range(5):
         print("Game", game + 1)
         print("Number must be between 1 and 5! \n")
-        guess_row = int(input("guess row: "))
-        guess_col = int(input("guess column: "))
+        guess_row = input("guess row: ")
+        guess_col = input("guess column: ")
         while guess_validate([guess_row, guess_col]) is False:
-            guess_row = int(input("guess row: "))
-            guess_col = int(input("guess column: "))
+            guess_row = input("guess row: ")
+            guess_col = input("guess column: ")
 
-        guess_row += -1
-        guess_col += -1
+        # guess_row += -1
+        # guess_col += -1
 
         if guess_row == ship_comp_row_num and guess_col == ship_comp_col_num:
             computer_board[guess_row][guess_col] = "o"
@@ -85,9 +85,8 @@ def guess_player_number(player_name):
             print_computer_board()
             break
         else:
-            if (guess_row < 0 or guess_row > 6) or \
-             (guess_col < 0 or guess_col > 6):
-                print("Wrong! number must be equal or smaller than 5!")
+            if guess_row not in range(5) and guess_col not in range(5):
+                print("Wrong! number must be equal or smaller than 4!")
             elif (computer_board[guess_row][guess_col] == "x"):
                 print("you hit that already")
             else:
@@ -107,17 +106,16 @@ def guess_computer_number():
     """
     Computer guess numbers to find the ship
     """
-    guess_row = random.randint(1, 6)
-    guess_col = random.randint(1, 6)
+    guess_row = random.randint(1, 5)
+    guess_col = random.randint(1, 5)
 
     if guess_row == ship_player_row_num and guess_col == ship_player_col_num:
         player_board[guess_row][guess_col] = "o"
         print_player_board()
         return True
     else:
-        if(guess_row < 0 or guess_row > 6) or \
-             (guess_col < 0 or guess_col > 6):
-            print("Wrong! number must be equal or smaller than 5!")
+        if guess_row not in range(5) or guess_col not in range(5):
+            print("Wrong! number must be equal or smaller than 4!")
         elif (player_board[guess_row][guess_col] == "z"):
             print("you hit that already")
         else:
@@ -133,33 +131,31 @@ def guess_validate(values):
     validator checks if it is an integer,
     if not ValueError
     """
-
     try:
         for value in values:
-            value == int(value)
+            value = int(value)
 
-            if (value < 1 or value > 6):
-                raise ValueError(f"Values must be between 1 and 5, \
-                 you provided {value}")
+            if (value < 1 or value > 5):
+                raise ValueError(
+                    f"Values must be between 1 and 5, you provided {value}"
+                )
     except ValueError as e:
-        print(f"Sorry that {e} is not a number, try again please")
+        print(f"Sorry: {e}, it is not a number, try again please!")
         return False
-
-    return True
 
 
 def main():
     """
     This is the function what runs the game
     """
-    wins = 0
-    losses = 0
     print("***********************************")
     print(colored("**Let's play the battle of ships!**", "yellow"))
+    print("***********************************")
     print("***********************************")
     print(colored("Game rules: ", "yellow"))
     print(colored("Two player game, human against the computer. Each player guess their number.", "yellow"))
     print(colored("Then the bets will be visible on the boards. Each player has 5 chance to bet", "yellow"))
+    print("***********************************")
     size = 5
     num_ships = 2
     game = 4
