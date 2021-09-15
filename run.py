7faf1e52ad555484c56309b0d8661fffb5a72a3c
Balazs9 <sempergerbalazs@gmail.com>
2021-09-15 08:17:37 +0000

    Add validate functions

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ import sys
 from termcolor import colored, cprint
 
 
-board = []
+player_board = []
+computer_board = []
 
 player = {
     "name": "player_name",
@@ -17,22 +18,23 @@ computer = {
     "lose": 0
 }
 for x in range(0, 5):
-    board.append(["."] * 5)
+    player_board.append(["."] * 5)
+    computer_board.append(["."] * 5)
 
 
-def print_player_board(board):
-    for row in board:
+def print_player_board():
+    for row in player_board:
         print((" ").join(row))
 
 
-def print_computer_board(board):
-    for row in board:
+def print_computer_board():
+    for row in computer_board:
         print((" ").join(row))
 
 
-print_player_board(board)
+print_player_board()
 print("---------")
-print_computer_board(board)
+print_computer_board()
 
 
 def random_row(board):
@@ -43,8 +45,10 @@ def random_col(board):
     return randint(0, len(board[0]) - 1)
 
 
-ship_row_num = random_row(board)
-ship_col_num = random_col(board)
+ship_comp_row_num = random_row(player_board)
+ship_comp_col_num = random_col(player_board)
+ship_player_row_num = random_row(computer_board)
+ship_player_col_num = random_col(computer_board)
 
 
 def take_turn(game):
@@ -63,59 +67,65 @@ def guess_player_number(player_name):
     """
     for game in range(5):
         print("Game", game + 1)
-        print("Number must be between 0 and 4! \n")
-        guess_row = int(input("guess row: "))
-        guess_col = int(input("guess column: "))
+        print("Number must be between 1 and 5! \n")
+        guess_row = input("guess row: ")
+        guess_col = input("guess column: ")
+        while guess_validate([guess_row, guess_col]) is False:
+            guess_row = input("guess row: ")
+            guess_col = input("guess column: ")
+
+        guess_row -= 1
+        guess_col -= 1
 
         # print(ship_row_num)
         # print(ship_col_num)
 
-        if guess_row == ship_row_num and guess_col == ship_col_num:
+        if guess_row == ship_comp_row_num and guess_col == ship_comp_col_num:
+            computer_board[guess_row][guess_col] = "o"
             print("Congratulations you found my ship!")
+            print_computer_board()
             break
         else:
             if (guess_row < 0 or guess_row > 4) or \
              (guess_col < 0 or guess_col > 4):
                 print("Wrong! number must be equal or smaller than 4!")
-            elif (board[guess_row][guess_col] == "x"):
+            elif (computer_board[guess_row][guess_col] == "x"):
                 print("you hit that already")
             else:
                 print("you missed my ship!")
-                board[guess_row][guess_col] = "x"
-            print_player_board(board)
+                computer_board[guess_row][guess_col] = "x"
+            print_computer_board()
+            computer_result = guess_computer_number()
+            if computer_result:
+                print("Computer has Won!")
+                break
             if game == 5:
                 print("Game over")
             game += 1
-            return guess_computer_number
 
 
 def guess_computer_number():
     """
     Computer guess numbers to find the ship
     """
-    for game in range(5):
-        print("Game", game + 1)
-        print("number must be between 0 and 4!")
-        guess_row = random.randint(0, 4)
-        guess_col = random.randint(0, 4)
+    guess_row = random.randint(0, 4)
+    guess_col = random.randint(0, 4)
 
-        if guess_row == ship_row_num and guess_col == ship_col_num:
-            print("Congratulations you found my ship!")
-            break
+    if guess_row == ship_player_row_num and guess_col == ship_player_col_num:
+        player_board[guess_row][guess_col] = "o"
+        print_player_board()
+        return True
+    else:
+        if(guess_row < 0 or guess_row > 4) or \
+            (guess_col < 0 or guess_col > 4):
+            print("Wrong! number must be equal or smaller than 4!")
+        elif (player_board[guess_row][guess_col] == "z"):
+            print("you hit that already")
         else:
-            if(guess_row < 0 or guess_row > 4) or \
-             (guess_col < 0 or guess_col > 4):
-                print("Wrong! number must be equal or smaller than 4!")
-            elif (board[guess_row][guess_col] == "z"):
-                print("you hit that already")
-            else:
-                print("you missed my ship!")
-                board[guess_row][guess_col] = "z"
-            print_computer_board(board)
-            if game == 5:
-                print("Game over")
-            game += 1
-            return guess_player_number
+            print("you missed my ship!")
+            player_board[guess_row][guess_col] = "z"
+        print_player_board()
+        return False
 
 
 def guess_validate(values):
@@ -126,10 +136,12 @@ def guess_validate(values):
     """
 
     try:
-        [int("value") for guess_row, guess_col in values]
-        if "value" != 5:
-            raise ValueError(f"Value must be smaller than 4, n\
-                 you provided {value}")
+        for value in values:
+            value = int(value)
+
+            if value < 1 or value > 6:
+                raise ValueError(f"Values must be between 1 and 5, \
+                    you provided {value}")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
